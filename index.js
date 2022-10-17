76d8da2f574a95e419b7e935f8c96faa37f7ebaf
Subham Dudheria <73893714+subham827@users.noreply.github.com>
2022-10-18 01:08:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 const cors = require('cors');
 const app = express();
 const articles  = [];
-const articles1 = [];
+
 const articles2 = [];
 let u =0;
 // set limit for articles to be scraped
@@ -21,62 +21,38 @@ const newspapers =[
     base: 'https://www.telegraph.co.uk'
 }
 ]
+newspapers.forEach((newspaper)=>{
+    axios.get(newspaper.url).then((response)=>{
+        const html = response.data;
+        const $ = cheerio.load(html);
+        $('a:contains("limate")',html).each(function(){
+            const title = $(this).text().replace("\n","").trim();
+            const link = $(this).attr('href');
+            // const image = $(this).find('img').attr('src');
+            articles.push({
+                title,
+                link : newspaper.base + link,
+                source : newspaper.name,
+                
+            })
+        })
+    })
+})
 app.get('/', (req, res) => {
     res.json('Welcome');
 })
 app.get('/news',(req,res)=>{
-    newspapers.forEach((newspaper)=>{
-        axios.get(newspaper.url).then((response)=>{
-            const html = response.data;
-            const $ = cheerio.load(html);
-            $('a:contains("limate")',html).each(function(){
-                const title = $(this).text().replace("\n","").trim();
-                const link = $(this).attr('href');
-                articles.push({
-                    title,
-                    link : newspaper.base + link,
-                    source : newspaper.name
-                })
-            })
-        })
-    })
     res.json(articles);
     console.log(articles.length);
     u = articles.length;
-    for(let i=0;i<articles.length;i++){
-        articles2[i] = articles[i];
-    }
+   
 })
 app.get('/news/:newspaperid', async(req,res)=>{
     if(req.params.newspaperid == 'guardian'){
-        const url = 'https://www.theguardian.com/environment/climate-crisis';
-        const html = await axios.get(url);
-        const $ = cheerio.load(html.data);
-        $('a:contains("limate")',html.data).each(function(){
-            const title = $(this).text().replace("\n","").trim();
-            const link = $(this).attr('href');
-            articles1.push({
-                title,
-                link ,
-                source : 'guardian'
-            })
-        })
-        res.json(articles1);
+      res.json(articles.filter((article)=>article.source == 'guardian'));
     }
     else{
-        const url = 'https://www.telegraph.co.uk/climate-change';
-        const html = await axios.get(url);
-        const $ = cheerio.load(html.data);
-        $('a:contains("limate")',html.data).each(function(){
-            const title = $(this).text().replace("\n","").trim();
-            const link = $(this).attr('href');
-            articles1.push({
-                title,
-                link  : 'https://www.telegraph.co.uk' + link,
-                source : 'telegraph'
-            })
-        })
-        res.json(articles1);
+        res.json(articles.filter((article)=>article.source == 'telegraph'));
     }
 })
 console.log(articles2.length);
@@ -84,11 +60,10 @@ app.get('/news/pages/:pageno', (req,res)=>{
     const pageno = req.params.pageno;
     // convert string to integer
     const page = parseInt(pageno);
-    for (let index = (page-1)*10; index < articles2.length; index++) {
-        const element = articles2[index];
-        res.json(element);
-        
-    }
+  // filter first 10 articles
+    const first10 = articles.slice((page-1)*10,page*10);
+    res.json(first10);
+
 })
 
-app.listen(5000, ()=> console.log("5000"))
+app.listen(5000, ()=> console.log("5000"));
